30a279fe78ab2cab6ce190232603a3187272a696
idlakshan <id.lakshan21@gmail.com>
2025-02-09 19:57:48 +0530

    feat: add inbuilt cp database

---
+++ src/main/java/com/mcc/backend/dto/UserDetailsDTO.java
@@ -0,0 +1,40 @@
+package com.mcc.backend.dto;
+
+public class UserDetailsDTO {
+    private int id;
+    private int user_Id;
+    private int role_id;
+
+    public UserDetailsDTO() {
+    }
+
+    public UserDetailsDTO(int id, int user_Id, int role_id) {
+        this.id = id;
+        this.user_Id = user_Id;
+        this.role_id = role_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_Id() {
+        return user_Id;
+    }
+
+    public void setUser_Id(int user_Id) {
+        this.user_Id = user_Id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+}
